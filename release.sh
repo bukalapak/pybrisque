cf06e9e08f0113da2ac7e49d0a42298f8afc598f
Akbar Gumbira <akbargumbira@gmail.com>
2018-08-08 17:25:38 +0700

    Update release script.

--- release.sh
+++ release.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
+
 echo "Have you updated changelog?"
 echo "Have you updated the version in setup.py?"
 echo "Have you updated README & Docs?"
 read -sn 1 -p "Press any key to continue..."
 echo
 
-VERSION=`cat setup.py | grep version | grep -o "[0-9}*\.[0-9]*\.[0-9]*"`
-TAG_VERSION=`echo $VERSION| sed 's/\./_/g'`
-echo "Version: " $VERSION
-echo "Tag Version: " $TAG_VERSION
-git tag -s version-$TAG_VERSION -m "Version $VERSION"
-git push --tags origin version-$TAG_VERSION
+python setup.py sdist bdist_wheel
 
-python setup.py sdist upload
+if [ $1 == "test" ];
+ then
+    echo TEST RELEASE;
+    twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+else
+    echo PROD RELEASE;
+
+    VERSION=`cat setup.py | grep version | grep -o "[0-9}*\.[0-9]*\.[0-9]*"`
+    TAG_VERSION=`echo $VERSION| sed 's/\./_/g'`
+    echo "Version: " $VERSION
+    echo "Tag Version: " $TAG_VERSION
+    git tag -s version-$TAG_VERSION -m "Version $VERSION"
+    git push --tags origin version-$TAG_VERSION
+
+    twine upload --repository-url https://upload.pypi.org/legacy dist/*
+fi
+
+
